ad77250ceb4b2789572819a4df29e5108e5d5be3
zq-bro3 <kusa1500@126.com>
2025-12-05 14:46:20 +0800

    Delete project directory

--- project/src/index.tsx
+++
@@ -1,9 +0,0 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import { Screen } from "./screens/Screen";
-
-createRoot(document.getElementById("app") as HTMLElement).render(
-  <StrictMode>
-    <Screen />
-  </StrictMode>,
-);
