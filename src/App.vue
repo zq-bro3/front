bf54037a2bdb60cc4a5ef9e5d55b44ab0051c7ee
zq-bro3 <kusa1500@126.com>
2025-12-05 14:11:11 +0800

    Added package.json

---
+++ src/App.vue
@@ -0,0 +1,7 @@
+<template>
+  <Screen />
+</template>
+
+<script setup lang="ts">
+import Screen from "./screens/Screen/Screen.vue";
+</script>
---
+++ src/components/ui/Input.vue
@@ -0,0 +1,28 @@
+<template>
+  <input
+    :type="type"
+    :class="
+      cn(
+        'flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-base shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 md:text-sm',
+        $attrs.class
+      )
+    "
+    v-bind="$attrs"
+  />
+</template>
+
+<script setup lang="ts">
+import { cn } from "../../lib/utils";
+
+interface InputProps {
+  type?: string;
+}
+
+withDefaults(defineProps<InputProps>(), {
+  type: "text",
+});
+
+defineOptions({
+  inheritAttrs: false,
+});
+</script>
